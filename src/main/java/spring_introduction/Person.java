bbce4022a6a4c30d91d297a5915e965fa15d7bda
Sanjar <sanjar171766@gmail.com>
2025-11-24 00:26:20 +0500

    009

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -3,14 +3,41 @@ package spring_introduction;
 public class Person {
 
     private Pet pet;
+    private String surname;
+    private int age;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
         System.out.println("Person bean is created");
-        this.pet = pet;
     }
 
     public void callYourPet() {
         System.out.println("Hello, my pet");
         pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 }
